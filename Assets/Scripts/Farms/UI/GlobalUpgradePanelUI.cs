9edc96ca34beeec45a15d1f20d9323fe21be4f82
Vũ Mạnh Hà <havm@tqcglobal.com>
2026-03-20 00:51:42 +0700

    Update GlobalUpgradePanelUI.cs

--- Assets/Scripts/Farms/UI/GlobalUpgradePanelUI.cs
+++ Assets/Scripts/Farms/UI/GlobalUpgradePanelUI.cs
@@ -1,9 +1,33 @@
+using Farms.Managers;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Farms.UI
 {
     public class GlobalUpgradePanelUI : MonoBehaviour
     {
-        
+        [SerializeField] private Button allPlantProfitButton;
+        [SerializeField] private Button addCustomerButton;
+
+        private GameManager _gameManager;
+
+        public void Init(GameManager gameManager)
+        {
+            _gameManager = gameManager;
+            allPlantProfitButton.onClick.AddListener(OnUpgradeAllPlantProfit);
+            addCustomerButton.onClick.AddListener(OnAddCustomer);
+        }
+
+        private void OnUpgradeAllPlantProfit()
+        {
+            _gameManager.UpgradeSystem.UpgradeAllPlantProfit(
+                _gameManager.RunTime.upgradeState, 2f);
+        }
+
+        private void OnAddCustomer()
+        {
+            _gameManager.UpgradeSystem.AddCustomers(
+                _gameManager.RunTime.upgradeState, 2);
+        }
     }
 }
